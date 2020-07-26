964d846010af6204f413d4d91f2df7fa58b011a8
soren@thomson <scott.soren@gmail.com>
2020-07-26 12:20:29 +0200

    update readme, add requirements and src

---
+++ src/ixdat/__init__.py
@@ -0,0 +1,8 @@
+__version__ = "0.0.1dev"
+__title__ = "ixdat"
+__description__ = "The in-situ experimental data tool"
+__url__ = "https://github.com/ixdat/ixdat"
+__author__ = "Soren B. Scott, Kevin Krempl, Kenneth Nielsen"
+__email__ = "scott.soren@gmail.com"  # maybe we should get an orgianization email?
+# __copyright__ = "Copyright (c) 2020 ixdat"
+__license__ = "MIT"
