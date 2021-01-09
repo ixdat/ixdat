4a007a0c87e9eb8d2cf6f5f59e263553470cf0f6
kkrempl <kevinkrempl@gmail.com>
2021-01-09 10:32:10 +0100

    EC_MS .pkl reader

--- src/ixdat/readers/ec_ms.py
+++ src/ixdat/readers/ec_ms.py
@@ -16,27 +16,42 @@ class EC_MS_CONVERTER:
             data = pickle.load(f)
 
         cols_str = data['data_cols']
-        data = Measurement("test",
-            technique="EC_MS",
-            )
-        for col in cols:
+        cols_list = []
 
+        for col in cols_str:
             if col[0] == "M" and col[-1] == "x":
-                cols_list.append(TimeSeries(col, "s", data[col], data["tstamp"]))
+                cols_list.append(TimeSeries(
+                    col, "s",
+                    data[col], data["tstamp"]
+                    ))
 
             if col == "time/s":
-                cols_list.append(TimeSeries(col, "s", data[col], data["tstamp"]))
+                cols_list.append(TimeSeries(
+                    col, "s",
+                    data[col], data["tstamp"]
+                    ))
 
-        for col in cols:
+        measurement = Measurement('tseries_ms',
+            technique = 'EC_MS',
+            series_list=cols_list)
 
+
+        for col in cols_str:
             if col[0] == "M" and col[-1] == "y":
-                cols_list.append(ValueSeries(col, "A", data[col], data["tstamp"], tseries=))
+                cols_list.append(ValueSeries(
+                col, "A",
+                data[col],
+                tseries=measurement[col[:-1] + 'x']
+                ))
+            if col == 'Ewe/V' or col == 'I/mA':
+                cols_list.append(ValueSeries(
+                col, "A",
+                data[col],
+                tseries=measurement['time/s']
+                ))
 
-            if col == "I/mA"
-                cols_list.append(ValueSeries(col, "mA", data[col], data["tstamp"]))
+        measurement = Measurement('tseries_ms',
+            technique = 'EC_MS',
+            series_list=cols_list)
 
-            if col == "Ewe/V"
-                cols_list.append(ValueSeries(col, "V", data[col], data["tstamp"]))
-
-
-        pass
+        return measurement
