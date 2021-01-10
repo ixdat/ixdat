fba0d54edb9182401c33a8815ece6720c3ccf919
ScottSoren <scott.soren@gmail.com>
2021-01-10 17:10:30 +0000

    move demo/test to bottom of biologic module

--- src/ixdat/readers/biologic.py
+++ src/ixdat/readers/biologic.py
@@ -1,4 +1,7 @@
-"""This module implements the Reader for .mpt files made by BioLogic's EC-Lab software"""
+"""This module implements the Reader for .mpt files made by BioLogic's EC-Lab software
+
+Demonstrated/tested at the bottom under `if __name__ == "__main__":`
+"""
 
 import re
 import time
@@ -225,3 +228,37 @@ def timestamp_string_to_tstamp(timestamp_string, form=timestamp_string_form):
     struct = time.strptime(timestamp_string, form)
     tstamp = time.mktime(struct)
     return tstamp
+
+
+if __name__ == "__main__":
+    """To run this module demo in PyCharm, configure it to run with 
+    Module name = ixdat.readers.biologic,
+    and *not* Script path = ...
+    """
+
+    from pathlib import Path
+    from matplotlib import pyplot as plt
+    from ixdat.measurements import Measurement
+
+    test_data_dir = Path(
+        __file__
+    ).parent.parent.parent.parent / "test_data/biologic_mpt_and_zilien_tsv"
+
+    path_to_test_file = (
+        test_data_dir / "2020-07-29 10_30_39 Pt_poly_cv_01_02_CVA_C01.mpt"
+    )
+
+    ec_measurement = Measurement.read(
+        reader="biologic",
+        path_to_file=path_to_test_file,
+    )
+
+    t, v = ec_measurement.get_potential(tspan=[0, 100])
+
+    ec_measurement.tstamp -= 20
+    t_shift, v_shift = ec_measurement.get_potential(tspan=[0, 100])
+
+    fig, ax = plt.subplots()
+    ax.plot(t, v, "k", label="original tstamp")
+    ax.plot(t_shift, v_shift, "r", label="shifted tstamp")
+    ax.legend()
