fix sign issue and v direction in sec

src/ixdat/readers/msrh_sec.py
@@ -45,11 +45,11 @@ class MsrhSECReader:
             data=np.array(ref_signal),
         )
 
-        v = jv_df["v"].to_numpy()
-        j = jv_df["j"].to_numpy()
-        excess_jv_points = len(v) - spectra.shape[0]
-        v = v[:-excess_jv_points]
-        j = j[:-excess_jv_points] * 1e3  # convert [A] to [mA]
+        v_0 = jv_df["v"].to_numpy()
+        j_0 = jv_df["j"].to_numpy()
+        # v = v[:-excess_jv_points]   # WRONG!!!
+        v = np.array([float(key) for key in sec_df.keys()])[1:]
+        j = np.flip(j_0)[-len(v) :]
         t = calc_t_using_scan_rate(v, dvdt=scan_rate * 1e-3)
 
         tstamp = tstamp or prompt_for_tstamp(path_to_file)

src/ixdat/techniques/spectroelectrochemistry.py
@@ -52,7 +52,7 @@ class SpectroECMeasurement(ECMeasurement):
             ref_counts = counts_interpolater(V_ref)
         else:
             ref_counts = self.reference_spectrum.y
-        dOD = np.log10(counts / ref_counts)
+        dOD = -np.log10(counts / ref_counts)  # check the sign!
         dOD_series = Field(
             name="$\Delta$ O.D.",
             unit_name="",
@@ -95,7 +95,7 @@ class SpectroECMeasurement(ECMeasurement):
             spectrum_ref = self.reference_spectrum
         spectrum = self.get_spectrum(V=V, t=t, index=index)
         field = Field(
-            data=np.log10(spectrum.y / spectrum_ref.y),
+            data=-np.log10(spectrum.y / spectrum_ref.y),
             name="$\Delta$ OD",
             unit_name="",
             axes_series=[self.wavelength],