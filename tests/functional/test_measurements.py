cb6f29119894e2ce9dfae814076db697779b7f81
Kenneth Nielsen <k.nielsen81@gmail.com>
2021-11-22 16:24:36 +0100

    Refactor existing tests to split them up into functional measurement and backends tests
    Also includes a test performance optimisation where the test Biologic file is reduced in size

--- tests/functional/test_measurements.py
+++ tests/functional/test_measurements.py
@@ -1,150 +1,96 @@
 """Tests that an ECMeasurement read from test data behaves as it should"""
-from pathlib import Path
-import time
+
+from pytest import approx
+
 from ixdat import Measurement
-from ixdat.db import change_database
+
 
 #  If tox crashes when trying to import matplotlib, see:
 #    https://github.com/ixdat/ixdat/issues/10
-# TODO: better tests!
-#   See https://github.com/ixdat/ixdat/pull/11#discussion_r747823991
 
-path_to_file = Path(__file__).parent.parent / "test_data/biologic/Pt_poly_cv.mpt"
-change_database("directory", project_name="test_biologic_ec_measurement")
-# TODO: Use a temporary directory.
-#   see: https://github.com/ixdat/ixdat/pull/11#discussion_r677567528
+# NOTE The `ec_measurement` and `fresh_directory_backend` arguments are provided by shared
+# fixtures in conftest.py in this directory
 
-print(f"cwd = {Path('.').absolute().resolve()}")
+class TestBiologicEC:
+    """This class tests the Biologic EC measurement"""
 
+    def test_basic_data(self, ec_measurement):
+        """Test basic data properties"""
+        assert ec_measurement.technique == "EC"
+        assert len(ec_measurement["potential"].data) == len(
+            ec_measurement["time/s"].data
+        )
 
-def test_append_essential_series():
-    # TODO: break this up into functions testing specific things.
-    meas = Measurement.read(path_to_file, reader="biologic")
-    print(f"successfully read {meas}")
+    def test_calibrate_and_append(self, ec_measurement):
+        """Test that measurement calibration works"""
+        ec_measurement.calibrate_RE(RE_vs_RHE=1)
+        assert ec_measurement.v[0] - ec_measurement["raw_potential"].data[0] == approx(
+            ec_measurement.RE_vs_RHE
+        )
 
-    assert meas.technique == "EC"
-    assert len(meas["potential"].data) == len(meas["time/s"].data)
+        # To make it complex, we first select a couple cycles, this time by converting to
+        # a cyclic voltammagram and indexing, and append them:
+        cv = ec_measurement.as_cv()
+        cvs_1_plus_2 = cv[1] + cv[2]
 
-    # Calibrate the potential and check that it works.
-    meas.calibrate_RE(RE_vs_RHE=1)
-    assert meas.v[0] - meas["raw_potential"].data[0] == meas.RE_vs_RHE
+        # Check that the calibration survived all that:
+        assert cvs_1_plus_2.RE_vs_RHE == ec_measurement.RE_vs_RHE
+        # Check that the main time variable, corresponding to potential, wasn't corrupted:
+        assert len(cvs_1_plus_2.grab("potential")[0]) == len(
+            cvs_1_plus_2["time/s"].data
+        )
+        # Check that the selector is still available and works with the main time variable:
+        assert len(cvs_1_plus_2.selector.data) == len(cvs_1_plus_2.t)
 
-    # To make it complex, we first select a couple cycles, this time by converting to
-    # a cyclic voltammagram and indexing, and append them:
-    cv = meas.as_cv()
-    cvs_1_plus_2 = cv[1] + cv[2]
+    def test_calibration_over_save_load(self, composed_measurement):
+        """Test save and load functionality"""
+        # Now let's try calibrating just the loaded one:
+        composed_measurement_copy = Measurement.from_dict(
+            composed_measurement.as_dict()
+        )
 
-    # Check that the calibration survived all that:
-    assert cvs_1_plus_2.RE_vs_RHE == meas.RE_vs_RHE
-    # Check that the main time variable, corresponding to potential, wasn't corrupted:
-    assert len(cvs_1_plus_2.grab("potential")[0]) == len(cvs_1_plus_2["time/s"].data)
-    # Check that the selector is still available and works with the main time variable:
-    assert len(cvs_1_plus_2.selector.data) == len(cvs_1_plus_2.t)
-    # ^ perfect! Tests that don't work now but should work after this restructure :D
+        RE_vs_RHE = 1
+        A_el = 2
+        composed_measurement_copy.calibrate(RE_vs_RHE=RE_vs_RHE, A_el=A_el)
 
+        # First we check that its v attribute is calibrated by comparing to the grabbed
+        # uncalibrated potential:
+        assert (
+            composed_measurement_copy.v[0]
+            == composed_measurement.grab("raw_potential")[1][0] + RE_vs_RHE
+        )
+        # ^ note this only builds the potential for cv12_loaded
 
-def test_save_load():
-    # TODO: break this up into functions testing specific things.
-    meas = Measurement.read(path_to_file, reader="biologic")
-    print(f"successfully read {meas}")
+        # First we check that its j attribute is calibrated by comparing to the same property
+        # of the original, still uncalibrated, measurement:
+        assert composed_measurement_copy.j[0] == composed_measurement.j[0] / A_el
+        # ^ note this builds the current for both cv12 and cv12_loaded
 
-    # To make it complex, we first select a couple cycles, this time with select(),
-    # and append them:
-    meas1 = meas.select(cycle=1)
-    meas2 = meas.select(cycle=3)
-    meas12 = meas1 + meas2
+        # As a result: there should be more cached series in cv12_loaded.
+        # However, the number of series in the series_list should be conserved:
+        assert len(composed_measurement.series_list) == len(
+            composed_measurement_copy.series_list
+        )
 
-    # Save and load the series:
-    i = meas12.save()
-    meas12_loaded = Measurement.get(i)
+        # Now, try copying the calibrated measurement by as_dict() and from_dict():
+        meas12_copied = Measurement.from_dict(composed_measurement_copy.as_dict())
+        # And check if it still has the calibration:
+        assert meas12_copied.A_el == A_el
+        # And that it can still apply it:
+        assert (
+            meas12_copied.grab("potential")[1][0]
+            == approx(composed_measurement.grab("potential")[1][0] + RE_vs_RHE)
+        )
 
-    # First, check that we have the full time vector and no more:
-    assert len(meas12.t) == len(meas12_loaded.t)
+        # Finally, save and load the copied measurement and check that it's still there:
+        # FIXME the load save test add to the total test time, this one is a primary
+        # candidate to be removed if we properly trust the backend tests
+        i2 = meas12_copied.save()
+        meas12_copied_loaded = Measurement.get(i2)
+        assert meas12_copied_loaded.RE_vs_RHE == RE_vs_RHE
+        assert len(meas12_copied_loaded.v) == len(composed_measurement.v)
 
-    # Now let's try calibrating just the loaded one:
-    RE_vs_RHE = 1
-    A_el = 2
-    meas12_loaded.calibrate(RE_vs_RHE=RE_vs_RHE, A_el=A_el)
-
-    # First we check that its v attribute is calibrated by comparing to the grabbed
-    #   uncalibrated potential:
-    assert meas12_loaded.v[0] == meas12.grab("raw_potential")[1][0] + RE_vs_RHE
-    # ^ note this only builds the potential for cv12_loaded
-    # First we check that its j attribute is calibrated by comparing to the same property
-    #   of the original, still uncalibrated, measurement:
-    assert meas12_loaded.j[0] == meas12.j[0] / A_el
-    # ^ note this builds the current for both cv12 and cv12_loaded
-
-    # As a result: there should be more cached series in cv12_loaded.
-    # However, the number of series in the series_list should be conserved:
-    assert len(meas12.series_list) == len(meas12_loaded.series_list)
-
-    # Now, try copying the calibrated meaurement by as_dict() and from_dict():
-    meas12_copied = Measurement.from_dict(meas12_loaded.as_dict())
-    # And check if it still has the calibration:
-    assert meas12_copied.A_el == A_el
-    # And that it can still apply it:
-    assert (
-        meas12_copied.grab("potential")[1][0]
-        == meas12.grab("potential")[1][0] + RE_vs_RHE
-    )
-
-    # Finally, save and load the copied measurement and check that it's still there:
-    i2 = meas12_copied.save()
-    meas12_copied_loaded = Measurement.get(i2)
-    assert meas12_copied_loaded.RE_vs_RHE == RE_vs_RHE
-    assert len(meas12_copied_loaded.v) == len(meas12.v)
-
-
-if __name__ == "__main__":
-    # TODO: probably doesn't beling here.
-    #   See https://github.com/ixdat/ixdat/pull/11#discussion_r677996529
-    meas = Measurement.read(path_to_file, reader="biologic")
-    print(f"successfully read {meas}")
-
-    # To make it complex, we first select a couple cycles, this time with select(),
-    # and append them:
-    meas1 = meas.select(cycle=1)
-    meas2 = meas.select(cycle=3)
-    meas12 = meas1 + meas2
-
-    # Save and load the series:
-    i = meas12.save()
-    meas12_loaded = Measurement.get(i)
-
-    # First, check that we have the full time vector and no more:
-    assert len(meas12.t) == len(meas12_loaded.t)
-
-    # Now let's try calibrating just the loaded one:
-    RE_vs_RHE = 1
-    A_el = 2
-    meas12_loaded.calibrate(RE_vs_RHE=RE_vs_RHE, A_el=A_el)
-
-    # First we check that its v attribute is calibrated by comparing to the grabbed
-    #   uncalibrated potential:
-    assert meas12_loaded.v[0] == meas12.grab("raw_potential")[1][0] + RE_vs_RHE
-    # ^ note this only builds the potential for cv12_loaded
-    # First we check that its j attribute is calibrated by comparing to the same property
-    #   of the original, still uncalibrated, measurement:
-    assert meas12_loaded.j[0] == meas12.j[0] / A_el
-    # ^ note this builds the current for both cv12 and cv12_loaded
-
-    # As a result: there should be more cached series in cv12_loaded.
-    # However, the number of series in the series_list should be conserved:
-    assert len(meas12.series_list) == len(meas12_loaded.series_list)
-
-    # Now, try copying the calibrated meaurement by as_dict() and from_dict():
-    meas12_copied = Measurement.from_dict(meas12_loaded.as_dict())
-    # And check if it still has the calibration:
-    assert meas12_copied.A_el == A_el
-    # And that it can still apply it:
-    assert (
-        meas12_copied.grab("potential")[1][0]
-        == meas12.grab("potential")[1][0] + RE_vs_RHE
-    )
-
-    # Finally, save and load the copied measurement and check that it's still there:
-    i2 = meas12_copied.save()
-    meas12_copied_loaded = Measurement.get(i2)
-    assert meas12_copied_loaded.RE_vs_RHE == RE_vs_RHE
-    assert len(meas12_copied_loaded.v) == len(meas12.v)
+    def test_to_and_as_dict(self, composed_measurement):
+        """Test a to_dict/from_dict round trip yields the same result"""
+        round_trip = Measurement.from_dict(composed_measurement.as_dict())
+        assert round_trip == composed_measurement
