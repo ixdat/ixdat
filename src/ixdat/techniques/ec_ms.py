9889bed71e2de00499594bf9511efb6ef4bef9ee
soren@thomson <scott.soren@gmail.com>
2022-02-06 13:09:52 +0000

    -- v0.1.8 -- fix ECMSCyclicVoltammogram inheritance

--- src/ixdat/techniques/ec_ms.py
+++ src/ixdat/techniques/ec_ms.py
@@ -212,81 +212,9 @@ class ECMSMeasurement(ECMeasurement, MSMeasurement):
         return cal
 
 
-class ECMSCyclicVoltammogram(CyclicVoltammogram, MSMeasurement):
-    """Class for raw EC-MS functionality. Parents: CyclicVoltammogram, MSMeasurement
-
-    FIXME: Maybe this class should instead inherit from ECMSMeasurement and
-        just add the CyclicVoltammogram functionality?
+class ECMSCyclicVoltammogram(CyclicVoltammogram, ECMSMeasurement):
+    """Class for raw EC-MS functionality. Parents: CyclicVoltammogram, ECMSMeasurement
     """
-
-    extra_column_attrs = {
-        # FIXME: It would be more elegant if this carried over from both parents
-        #   That might require some custom inheritance definition...
-        "ecms_meaurements": {
-            "mass_aliases",
-            "signal_bgs",
-            "ec_technique",
-            "RE_vs_RHE",
-            "R_Ohm",
-            "raw_potential_names",
-            "A_el",
-            "raw_current_names",
-        },
-    }
-
-    def __init__(self, **kwargs):
-        """FIXME: Passing the right key-word arguments on is a mess"""
-        ec_kwargs = {
-            k: v for k, v in kwargs.items() if k in ECMeasurement.get_all_column_attrs()
-        }
-        ec_kwargs.update(series_list=kwargs["series_list"])
-        ECMeasurement.__init__(self, **ec_kwargs)
-        ms_kwargs = {
-            k: v for k, v in kwargs.items() if k in MSMeasurement.get_all_column_attrs()
-        }
-        ms_kwargs.update(series_list=kwargs["series_list"])
-        MSMeasurement.__init__(self, **ms_kwargs)
-        self.plot = self.plotter.plot_vs_potential
-        # FIXME: only necessary because an ECMSCalibration is not seriealizeable.
-        self.calibration = kwargs.get("calibration", None)
-
-    @property
-    def plotter(self):
-        """The default plotter for ECMSCyclicVoltammogram is ECMSPlotter"""
-        if not self._plotter:
-            from ..plotters.ecms_plotter import ECMSPlotter
-
-            self._plotter = ECMSPlotter(measurement=self)
-
-        return self._plotter
-
-    @property
-    def exporter(self):
-        """The default plotter for ECMSCyclicVoltammogram is ECMSExporter"""
-        if not self._exporter:
-            self._exporter = ECMSExporter(measurement=self)
-        return self._exporter
-
-    def as_dict(self):
-        self_as_dict = super().as_dict()
-
-        if self.calibration:
-            self_as_dict["calibration"] = self.calibration.as_dict()
-            # FIXME: now that ECMSCalibration should be seriealizeable, it could
-            #  go into extra_column_attrs. But it should be a reference.
-        return self_as_dict
-
-    @classmethod
-    def from_dict(cls, obj_as_dict):
-        """Unpack the ECMSCalibration when initiating from a dict"""
-        if "calibration" in obj_as_dict:
-            if isinstance(obj_as_dict["calibration"], dict):
-                # FIXME: This is a mess
-                obj_as_dict["calibration"] = ECMSCalibration.from_dict(
-                    obj_as_dict["calibration"]
-                )
-        obj = super(ECMSCyclicVoltammogram, cls).from_dict(obj_as_dict)
-        return obj
 
 
 class ECMSCalibration(Saveable):
