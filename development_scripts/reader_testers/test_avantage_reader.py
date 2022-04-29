9e3febac50ef55128e815508c3bb12058f8d5403
ScottSoren <sbscott@ic.ac.uk>
2022-04-29 17:30:08 +0100

    avantage reader with old 2019 pyThetaProbe code

--- development_scripts/reader_testers/test_avantage_reader.py
+++ development_scripts/reader_testers/test_avantage_reader.py
@@ -3,9 +3,7 @@ from pathlib import Path
 from ixdat import Spectrum
 
 path_to_file = (
-    Path.home()
-    / "Dropbox/ixdat_resources/test_data/avantage"
-    / "XPS Survey.avg"
+    Path.home() / "Dropbox/ixdat_resources/test_data/avantage" / "XPS Survey.avg"
 )
 
 spectrum = Spectrum.read(path_to_file, reader="avantage")
