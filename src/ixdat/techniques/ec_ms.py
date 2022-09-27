fe605176a7aec5762e081872ba05337e37c573dd
ScottSoren <sbscott@ic.ac.uk>
2022-09-27 14:53:51 +0100

    si quant ecms calibration

--- src/ixdat/techniques/ec_ms.py
+++ src/ixdat/techniques/ec_ms.py
@@ -7,6 +7,7 @@ from .cv import CyclicVoltammogram
 from ..exporters.ecms_exporter import ECMSExporter
 from ..plotters.ecms_plotter import ECMSPlotter
 from ..plotters.ms_plotter import STANDARD_COLORS
+from ..config import plugins
 
 
 class ECMSMeasurement(ECMeasurement, MSMeasurement):
@@ -185,13 +186,23 @@ class ECMSMeasurement(ECMeasurement, MSMeasurement):
             n_fit = np.array([0, max(n_vec)])
             Y_fit = n_fit * pfit[0] + pfit[1]
             ax.plot(n_fit * 1e9, Y_fit * 1e9, "--", color=color)
-        cal = MSCalResult(
-            name=f"{mol}@{mass}",
-            mol=mol,
-            mass=mass,
-            cal_type="ecms_calibration_curve",
-            F=F,
-        )
+
+        if plugins.use_si_quant:
+            cal = plugins.si_quant.CalPoint(
+                mol=mol,
+                mass=mass,
+                F_type="internal",
+                F=F,
+            )
+        else:
+            cal = MSCalResult(
+                name=f"{mol}@{mass}",
+                mol=mol,
+                mass=mass,
+                cal_type="ecms_calibration_curve",
+                F=F,
+            )
+
         if return_ax:
             return cal, ax
         return cal
@@ -218,6 +229,7 @@ class ECMSMeasurement(ECMeasurement, MSMeasurement):
 
         Returns tspan_list(list of tspan)
         """
+        selector_name = selector_name or "selector"
         t_idx = -1
         if not t_steady_pulse:
             t_idx = 0
