6b88ba78ecc54e4184e6a87e94032aeca25080b2
AlexanderKrabbe <alexander@krabbe.me>
2023-01-26 17:32:19 +0100

    removed redudandent code

--- src/ixdat/plotters/tpms_plotter.py
+++ src/ixdat/plotters/tpms_plotter.py
@@ -231,12 +231,6 @@ class TPMSPlotter(MPLPlotter):
                 legend=legend,
                 **kwargs,
             )
-            for mass_label in [mass_list, mass_lists, mol_list, mol_lists]:
-                print(mass_label)
-                t, x = measurement.grab(T_name, tspan=tspan)
-            # t_v, v = measurement.grab(mass, tspan=tspan)
-
-            # axes.plot(t_v,v, label=mass)
 
         return axes
 
