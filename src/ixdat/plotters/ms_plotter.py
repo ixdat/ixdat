fb7b7724b9b058c4a3347cfd855d53fd226812f0
AlexanderKrabbe <alexander@krabbe.me>
2023-01-30 00:08:39 +0100

    added docstrings, changed _sort_indicies to sort_spectra, elaborated, described ms_data='top'

--- src/ixdat/plotters/ms_plotter.py
+++ src/ixdat/plotters/ms_plotter.py
@@ -453,17 +453,19 @@ class MSPlotter(MPLPlotter):
                 "pmol/s": 1e12,
                 "nmol/s": 1e9,
                 "umol/s": 1e6,
+                "mmol/s": 1e3,
                 "mol/s": 1,  # noqa
                 "pmol/s/cm^2": 1e12,
                 "nmol/s/cm^2": 1e9,
                 "umol/s/cm^2": 1e6,
+                "mmol/s/cm^2": 1e3,
                 "mol/s/cm^2": 1,  # noqa
             }[unit]
             if "/cm^2" in unit:
                 unit_factor = unit_factor / measurement.A_el
         else:
             unit = unit or "A"
-            unit_factor = {"pA": 1e12, "nA": 1e9, "uA": 1e6, "A": 1}[unit]
+            unit_factor = {"pA": 1e12, "nA": 1e9, "uA": 1e6, "mA": 1e3, "A": 1}[unit]
         # TODO: Real units with a unit module! This should even be able to figure out the
         #  unit prefix to put stuff in a nice 1-to-1e3 range
 
@@ -500,7 +502,9 @@ class SpectroMSPlotter(MPLPlotter):
         tspan_bg=None,
         remove_background=None,
         unit=None,
+        x_unit=None,
         logplot=True,
+        logdata=False,
         legend=True,
         xspan=None,
         cmap_name="inferno",
@@ -510,8 +514,71 @@ class SpectroMSPlotter(MPLPlotter):
         max_threshold=None,
         min_threshold=None,
         scanning_mask=None,
+        vmin=None,
+        vmax=None,
         **kwargs,
     ):
+        """Plot m/z signal, mass spectra vs time (MID) data and return the axes of a two
+        panel figure.
+
+        There are four ways to specify what to plot. Only specify one of these::
+            mass_list: Uncalibrated signals in [(u/n/p)A] on on axis
+            mass_lists: Uncalibrated signals in [(u/n/p)A] on two axes
+            mol_list: Calibrated signals in [(u/n/p)mol/s] on on axis
+            mol_lists: Calibrated signals in [(u/n/p)mol/s] on two axes
+
+        Two axes refers to separate left and right y-axes. Default is to use all
+        available masses as mass_list.
+
+        Args:
+            measurement (SpectroMSMeasurement): Defaults to the one that initiated the
+                plotter
+            axes (list of matplotlib axis): Left and right y-axes if mass_lists are given
+                default to axes[0] as left and axes[2] as right axis for plotting masses
+                and axes[1] for plotting MSSpectra 
+            mass_list (list of str): The names of the m/z values, eg. ["M2", ...] to
+                plot. Defaults to all of them (measurement.mass_list)
+            mass_lists (list of list of str): Alternately, two lists can be given for
+                masses in which case one list is plotted on the left y-axis and the other
+                on the right y-axis of the top panel.
+            mol_list (list of str): The names of the molecules, eg. ["H2", ...] to
+                plot. Defaults to all of them (measurement.mass_list)
+            mol_lists (list of list of str): Alternately, two lists can be given for
+                molecules in which case one list is plotted on the left y-axis and the
+                other on the right y-axis of the top panel.
+            tspan (iter of float): The time interval to plot, wrt measurement.tstamp
+            tspan_bg (timespan): A timespan for which to assume the signal is at its
+                background. The average signals during this timespan are subtracted.
+                If `mass_lists` are given rather than a single `mass_list`, `tspan_bg`
+                must also be two timespans - one for each axis. Default is `None` for no
+                background subtraction.
+            remove_background (bool): Whether otherwise to subtract pre-determined
+                background signals if available. Defaults to (not logplot)
+            unit (str): defaults to "A" or "mol/s"
+            x_unit (str): defaults to "s"
+            logplot (bool): Whether to plot the MS data on a log scale (default True)
+            logdata (bool): Whether to plot the natural logarithm of MS data on a
+                linear scale (default False)
+            legend (bool): Whether to use a legend for the MS data (default True)
+            xspan (iter of float): The physical span for spectra to plot
+            cmap_name (str): Colour map to pass to heat_plot method
+            make_colorbar (bool): Include a colour bar. Misalignes time axis with other
+                panels in same figure
+            emphasis (str): Whether to emphasise top or bottom panel 3/5 fig size or equal
+            ms_data (str): Whether to plot ms_data on "top" or "bottom" panel
+            max_threshold (int or float): Only applies to spectra plotted with heat_plot.
+                All values above max threshold is set to 0 (zero).
+            min_threshold (int or float): Only applies to spectra plotted with heat_plot.
+                All values below threshold is set to 0 (zero).
+            scanning_mask (boolean list): Only applies to spectra plotted with heat_plot.
+                List of booleans of same shape as SpectrumSeries.data to exclude specific
+                data prior to plotting
+            vmin (int or float): Shift minimum value in color bar. Default lowest value
+                in measurement.spectrum_series.
+            vmax (int or float): Shift maximum value in color bar. Default highest value
+                in measurement.spectrum_series.
+            kwargs: extra key-word args are passed on to matplotlib's plot()
+        """
 
         if logplot is None:
             logplot = not mol_lists and not mass_lists
@@ -557,11 +624,14 @@ class SpectroMSPlotter(MPLPlotter):
                 mol_list=mol_list,
                 mol_lists=mol_lists,
                 unit=unit,
+                x_unit=x_unit,
                 logplot=logplot,
+                logdata=logdata,
                 legend=legend,
                 **kwargs,
             )
 
+        # then we have the spectrum series to plot
         measurement.spectrum_series.heat_plot(
             ax=axes[ms_spec_axes],
             tspan=tspan,
@@ -571,6 +641,8 @@ class SpectroMSPlotter(MPLPlotter):
             max_threshold=max_threshold,
             min_threshold=min_threshold,
             scanning_mask=scanning_mask,
+            vmin=vmin,
+            vmax=vmax,
         )
 
         return axes
@@ -578,30 +650,130 @@ class SpectroMSPlotter(MPLPlotter):
     def plot_measurement_vs(
         self,
         *,
-        x_name,
+        v_name,
         measurement=None,
         axes=None,
         mass_list=None,
         mass_lists=None,
         mol_list=None,
         mol_lists=None,
+        vspan=None,
         tspan=None,
         tspan_bg=None,
         remove_background=None,
         unit=None,
+        x_unit=None,
         logplot=True,
+        logdata=False,
         legend=True,
-        xspan=None,
+        scan_span=None,
         cmap_name="inferno",
         make_colorbar=False,
+        vmin=None,
+        vmax=None,
         emphasis="top",
         ms_data="top",
         max_threshold=None,
         min_threshold=None,
         scanning_mask=None,
-        _sort_indicies=None,
+        sort_spectra='linear',
         **kwargs,
     ):
+        """Plot m/z signal and MSSpectra data in a two panel subfigure vs a specified
+        variable and return the axes.
+
+        There are four ways to specify which (MID) signals to plot in panel.
+        Only specify one of these:
+            mass_list: Uncalibrated signals in [(u/n/p)A] on on axis
+            mass_lists: Uncalibrated signals in [(u/n/p)A] on two axes
+            mol_list: Calibrated signals in [(u/n/p)mol/s] on on axis
+            mol_lists: Calibrated signals in [(u/n/p)mol/s] on two axes
+
+        Two axes refers to separate left and right y-axes. Default is to use all
+        available masses as mass_list.
+
+
+        Args:
+            measurement (SpectroMSMeasurement): Defaults to the one that initiated the
+                plotter
+            v_name (str): Name of the series to plot versus.
+            axes (list of matplotlib axis): Defaults to axes[0], axes[2] for left and
+                right axis for plotting masses and axes[1] for MSSpectra data.
+            mass_list (list of str): The names of the m/z values, eg. ["M2", ...] to
+                plot. Defaults to all of them (measurement.mass_list)
+            mass_lists (list of list of str): Alternately, two lists can be given for
+                masses in which case one list is plotted on the left y-axis and the other
+                on the right y-axis of the top panel.
+            mol_list (list of str): The names of the molecules, eg. ["H2", ...] to
+                plot. Defaults to all of them (measurement.mass_list)
+            mol_lists (list of list of str): Alternately, two lists can be given for
+                molecules in which case one list is plotted on the left y-axis and the
+                other on the right y-axis of the top panel.
+            vspan (iter of float): The value interval to plot on x_axis
+            tspan (iter of float): The time interval to plot, wrt measurement.tstamp
+            tspan_bg (timespan): A timespan for which to assume the signal is at its
+                background. The average signals during this timespan are subtracted.
+                If `mass_lists` are given rather than a single `mass_list`, `tspan_bg`
+                must also be two timespans - one for each axis. Default is `None` for no
+                background subtraction.
+            remove_background (bool): Whether otherwise to subtract pre-determined
+                background signals if available. Defaults to (not logplot)
+            unit (str): defaults to "A" or "mol/s"
+            x_unit (str): defaults to v_name.unit.name
+            logplot (bool): Whether to plot the MS data on a log scale (default True)
+            logdata (bool): Whether to plot the natural logarithm of MS data on a
+                linear scale (default False)
+            legend (bool): Whether to use a legend for the MS data (default True)
+            scan_span (iter of float): The physical span for spectra to plot
+            cmap_name (str): Colour map to pass to heat_plot method
+            make_colorbar (bool): Include a colour bar. Misalignes time axis with other
+                panels in same figure
+            vmin (int or float): Shift minimum value in color bar. Default lowest value
+                in measurement.spectrum_series.
+            vmax (int or float): Shift maximum value in color bar. Default highest value
+                in measurement.spectrum_series.
+            emphasis (str): Whether to emphasise top or bottom panel 3/5 fig size or equal
+                Default 'top'
+            ms_data (str): Whether to plot ms_data on "top" or "bottom" panel.
+                Default 'top'
+            max_threshold (int or float): Only applies to spectra plotted with heat_plot.
+                All values above max threshold is set to 0 (zero).
+            min_threshold (int or float): Only applies to spectra plotted with heat_plot.
+                All values below threshold is set to 0 (zero).
+            scanning_mask (boolean list): Only applies to spectra plotted with heat_plot.
+                List of booleans of same shape as SpectrumSeries.data to exclude specific
+                data prior to plotting
+            sort_spectra (list or str): Whether or not to sort the spectra data prior to
+                plotting.
+                There is three specifers:
+                    'none':
+                        This gives no new sorting. Effectively the spectras are sorted
+                        by time. (tstamp for each of the spectrum).
+                    'linear' (default):
+                        the spectras are sorted linear to v_name from low to high.
+                    a list of same shape as field.data to be sorted:
+                        This list is passed directly as the indices to sort the spectras.
+                        Defaults to sort lowest to highest value. Example
+                Note: If tspan spans a time span of a measurement with up and down
+                cycles in v_name, this might yield funny looking heat_plots.
+
+                    Example:
+                    Scanning up and down in temperature from T_low to T_high the spectras
+                    obtained wil be plotted from [T_low_start ..  T_high .. T_low_end].
+
+                    - If 'none' sorting is specified leads to heat_plot xaxis linearly from
+                    T_low_start to T_low_end missing representation of the high values in
+                    the middle of the axis.
+
+                    - If 'linear' sorting is specified all spectras obtained
+                        are sorted linearly from lowest v_name_value to highest v_name.
+                        When data is assymetric from scanning up or down in v_name this
+                        leads to abrupt looking figures since two non similair spectras
+                        are obtained at similair v_name value and hence plotted next to
+                        eachother.
+
+            kwargs: extra key-word args are passed on to matplotlib's plot()
+        """
 
         if logplot is None:
             logplot = not mol_lists and not mass_lists
@@ -635,7 +807,7 @@ class SpectroMSPlotter(MPLPlotter):
         ):
             # then we have MS data!
             self.ms_plotter.plot_vs(
-                x_name=x_name,
+                x_name=v_name,
                 measurement=measurement,
                 axes=[axes[ms_axes], axes[2]]
                 if (mass_lists or mol_lists)
@@ -648,29 +820,91 @@ class SpectroMSPlotter(MPLPlotter):
                 mol_list=mol_list,
                 mol_lists=mol_lists,
                 unit=unit,
+                x_unit=unit,
                 logplot=logplot,
+                logdata=logdata,
                 legend=legend,
                 **kwargs,
             )
 
-        _tseries = measurement.spectrum_series.field.axes_series[0]
-        _v = measurement.grab_for_t(item=x_name, t=_tseries.t)
+        # To plot heat plot.
+        # First get all values for v_name at all spectrum times
+        _t = measurement.spectrum_series.field.axes_series[0].t
+        _v = measurement.grab_for_t(item=v_name, t=_t)
 
-        if not _sort_indicies:
-            _sort_indicies = np.argsort(_v)
+        # find all t_indicies in original data set for later sorting
+        t_indicies = (np.array([i for i, v in enumerate(_v)]), )
 
+        if tspan:
+            # FIXME find a more elegant numpy way of extracting indicies directly from
+            # data in 
+            t_mask = np.logical_and(tspan[0] < _t, _t < tspan[-1])
+            ## t_indicies is the indicies of the total data set recorded within tspan
+            t_indicies = np.where(t_mask == True)
+            # _v is the values of v_name recorded at t within tspan (t_mask)
+            _v = _v[t_mask]
+
+        if isinstance(sort_spectra, str):
+            # t_sorted_indicies are the indicies of the sliced spectras data-set recorded
+            # wihtin tspan and sorted vs v_name according to sort_spectra
+            if sort_spectra == 'linear':
+                t_sorted_indicies = np.argsort(_v)
+
+            elif sort_spectra == 'none':
+                t_sorted_indicies = [i for i, v in enumerate(_v)]
+
+            else:
+                warnings.warn(
+                        f"Recieved {sort_spectra} for sort_spectra."
+                        "sort_spectra has to be 'linear',  'none' or "
+                        "of type list with same length as spectrum_series.",
+                        stacklevel = 2
+                        )
+            # Indicies of the total field.data where tstamp of spectra is within tspan and
+            # indicies are sorted versus v_name
+            _sorted_indicies = t_indicies[0][t_sorted_indicies]
+
+        elif isinstance(sort_spectra, list):
+            if len(_t) == len(sort_spectra):
+                _sorted_indicies = sort_spectra
+            else:
+                warnings.warn(
+                        f"length [{len(sort_spectra)}] of 'sort_spectra' has to be equal"
+                        f"to [{len(_t)}]."
+                        "sort_spectra can be 'linear', 'none' or "
+                        "of type list with same length as spectrum_series.",
+                        stacklevel = 2
+                        )
+        else:
+            warnings.warn(
+                        "sort_spectra has to be 'linear', 'none' or "
+                        "of type list with same length as spectrum_series.",
+                        stacklevel = 2
+                        )
+
+        # Now we can plot the heat plot
         measurement.spectrum_series.heat_plot(
             ax=axes[ms_spec_axes],
-            t=_v[_sort_indicies],
-            tspan=tspan,
-            xspan=xspan,
+            t=_v[t_sorted_indicies],  # So x_axis is sorted eqaul to the data
+            tspan=vspan,
+            xspan=scan_span,
             cmap_name=cmap_name,
             make_colorbar=make_colorbar,
+            vmin=vmin,
+            vmax=vmax,
             max_threshold=max_threshold,
             min_threshold=min_threshold,
             scanning_mask=scanning_mask,
-            _sort_indicies=_sort_indicies,
+            _sort_indicies=_sorted_indicies,
         )
+
+        axes[ms_spec_axes].set_xlim(axes[ms_axes].get_xlim())
+
+        if vspan:
+
+            axes[ms_axes].set_xlim([vspan[0], vspan[-1]])
+            axes[ms_spec_axes].set_xlim([vspan[0], vspan[-1]])
+        axes[ms_spec_axes].set_xlabel(v_name)
 
         return axes
 
