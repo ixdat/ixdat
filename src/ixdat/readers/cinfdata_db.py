0dda6920ba6b04234d2160ca453e241ec32795fd
AlexanderKrabbe <alexander@krabbe.me>
2023-03-21 21:13:42 +0100

    added comment about return from cinfdata

--- src/ixdat/readers/cinfdata_db.py
+++ src/ixdat/readers/cinfdata_db.py
@@ -206,8 +206,8 @@ class CinfdataDBReader:
                possible to combine in a SpectrumSeries, return self.spectrum_list
         """
         db = plugins.cinfdata.connect(setup_name=self.setup_name, grouping_column=self.grouping_column)
-        self.group_data = db.get_data_group(self.token)
-        self.group_meta = db.get_metadata_group(self.token)
+        self.group_data = db.get_data_group(self.token)  # return dict with uniqe measurement as the key containing x,y values in a np.array
+        self.group_meta = db.get_metadata_group(self.token)  # return dict of meta data associated with the measurement
         self.meta = self.group_meta[list(self.group_meta.keys())[0]]
 
         self.set_sample_name()
@@ -215,13 +215,13 @@ class CinfdataDBReader:
         self.set_tstamp()
 
         self.spectrum_list = []
-        for key in self.group_meta:
+        for key in self.group_meta:  # key is unique to each measurement
             group_type = self.group_meta[key]["type"]
             if group_type == 2:  # type 2 is unique describing XPS data
                 obj_as_dict = self.get_spectrum_as_obj(key, group_type)
                 obj_as_dict["name"] = self.group_meta[key]["name"]
                 self.spectrum_list.append(self.measurement_class.from_dict(obj_as_dict))
-            elif group_type == 4:  # type 4 is unique describing msscan
+            elif group_type == 4:  # type 4 is unique describing mass scan
                 obj_as_dict = self.get_spectrum_as_obj(key, group_type)
                 obj_as_dict["name"] = self.sample_name
                 self.spectrum_list.append(self.measurement_class.from_dict(obj_as_dict))
