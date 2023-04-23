05b548704a466fdf645e56df409fcd80d5dfd8ec
AlexanderKrabbe <alexander@krabbe.me>
2023-04-23 11:28:32 +0200

    cleand up _Cinfdata class in config.py

--- src/ixdat/config.py
+++ src/ixdat/config.py
@@ -181,58 +181,47 @@ class _SIQuantDeps:
         if self._USE_QUANT:
             self._USE_QUANT = True  # gets the quant directory to be reset
 
-class _CinfData:
 
-    def __init__(self,):
+class _CinfData:
+    """Class implement direct database read access using external module Cinfdata"""
+
+    def __init__(
+        self,
+    ):
         self.cinfdata = None
         self._managed_cinfdata_object = None
         self._context_manager_kwargs = None
-        print('initialised cinfdatabase connection')
 
     def activate(self):
-        print('activated cinfdatabase connection')
         from cinfdata import Cinfdata
 
         self.DB = Cinfdata
 
     def connect(self, setup_name=None, grouping_column=None):
-        print('connected cinfdatabase connection')
+        """setup_name (str): The name of the table inside the database
+        grouping_column (str): Either the 'timestamp'/'comment' or 'Comment' column"""
         return self.DB(setup_name=setup_name, grouping_column=grouping_column)
 
     def __call__(self, **kwargs):
-        """... arguments here same as `connect`"""
+        """**kwargs: setup_name (str) and grouping_column (str) (see connect())"""
         self._context_manager_kwargs = kwargs
-        print('called initialised cinfdatabase connection')
         return self
 
     def __enter__(self):
-        print('entered initialised cinfdatabase connection')
         if self._managed_cinfdata_object:
             self._context_manager_kwargs = None
-            raise RuntimeError("Using the cinfdata plugin as a context manager more than once at the same time is not supported")
+            raise RuntimeError(
+                "Using the cinfdata plugin as a context manager more "
+                "than once at the same time is not supported"
+            )
 
         self._managed_cinfdata_object = self.connect(**self._context_manager_kwargs)
         return self._managed_cinfdata_object
 
     def __exit__(self, exc_type, exc_val, exc_tb):
-        print('exited initialised cinfdatabase connection')
         self._managed_cinfdata_object.connection.close()
         self._managed_cinfdata_object = None
         self._context_manager_kwargs = None
-
-#class _CinfData:
-    #def __init__(
-        #self,
-    #):
-        #self.cinfdata = None
-
-    #def activate(self):
-        #from cinfdata import Cinfdata
-
-        #self.DB = Cinfdata
-
-    #def connect(self, setup_name=None, grouping_column=None):
-        #return self.DB(setup_name=setup_name, grouping_column=grouping_column)
 
 
 plugins = _PluginOptions()
