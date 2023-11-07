089f4113bef7ee1467c25bd7e600d640981961ce
scott <sbs@chem.ku.dk>
2023-11-07 18:37:58 +0100

    black format one file. annoying.

--- development_scripts/reader_demonstrators/demo_zilien_spectrum_reader.py
+++ development_scripts/reader_demonstrators/demo_zilien_spectrum_reader.py
@@ -4,7 +4,8 @@ from pathlib import Path
 from ixdat import Spectrum
 
 path_to_file = (
-    Path.home() / "Dropbox/ixdat_resources/test_data/zilien_spectra"
+    Path.home()
+    / "Dropbox/ixdat_resources/test_data/zilien_spectra"
     / "mass scan started at measurement time 0001700.tsv"
 )
 
