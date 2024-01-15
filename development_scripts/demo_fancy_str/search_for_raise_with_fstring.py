c5d349dd99a09543d7cbf1ed88aa65a9d23e82c7
Kenneth Nielsen <k.nielsen81@gmail.com>
2024-01-15 11:11:01 +0100

    Add comments to script to search for f-strings in exception strings

--- development_scripts/demo_fancy_str/search_for_raise_with_fstring.py
+++ development_scripts/demo_fancy_str/search_for_raise_with_fstring.py
@@ -1,9 +1,13 @@
+"""This file will look for f-strings in exceptions messages of any .py files **in the folder it is
+placed in** or its sub folders."""
+
 import re
 from pathlib import Path
 
 THIS_DIR = Path(__file__).resolve().parent
 
 
+# Look for the 'raise' keyword and capture anything in parentheses (non greedily) after that keyword
 RAISE = re.compile("raise .*?\([\s\S]*?\)")
 
 
@@ -18,6 +22,7 @@ for f in THIS_DIR.glob("**/*"):
     search_res = RAISE.findall(content)
     good_matches = []
     if search_res:
+        # Look for the f-string marker in the found exceptions strings
         for m in search_res:
             if 'f"' in m:
                 good_matches.append(m)
