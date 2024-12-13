f9717a242f2f032e8db2634d445142910edd28fc
Gullaschen <gustav.hedemark@hotmail.com>
2024-12-13 14:37:03 +0100

    fix flakes8 errors

--- development_scripts/reader_demonstrators/demo_ocean_reader.py
+++ development_scripts/reader_demonstrators/demo_ocean_reader.py
@@ -4,7 +4,8 @@ import matplotlib.pyplot as plt
 
 data_dir = Path.home() / r"Desktop\Thesis\Data\Sample 45"
 
-my_spec = Spectrum.read(data_dir / "Absorbance__0__11-24-39-251.txt",reader="ocean")
+my_spec = Spectrum.read(data_dir / "Absorbance__0__11-24-39-251.txt",
+                        reader="ocean")
 
 ax = my_spec.plot()
 
--- src/ixdat/readers/ocean.py
+++ src/ixdat/readers/ocean.py
@@ -2,16 +2,20 @@ import pandas as pd
 from ..data_series import Field, DataSeries
 
 
-class OceanTxTReader(): # in-development
-    """ 
-    Class for reading .txt files obtained from Ocean Optics spectrometers. 
+class OceanTxTReader():
+    """
+    Class for reading .txt files obtained from Ocean Optics spectrometers.
     """
     def read(self, path_to_file, cls=None, **kwargs):
-        df = pd.read_csv(path_to_file, sep="\t", skiprows=14, names=["Wavelength", "Absorbance"])
-        df["Wavelength"] = df["Wavelength"].str.replace(',', '.').astype(float) # replace commas with periods
+        df = pd.read_csv(path_to_file, sep="\t",
+                         skiprows=14, names=["Wavelength", "Absorbance"])
+
+        df["Wavelength"] = df["Wavelength"].str.replace(',', '.').astype(float)
         df["Absorbance"] = df["Absorbance"].str.replace(',', '.').astype(float)
-        
-        wl = DataSeries(name="Wavelength", unit_name="nm", data=df["Wavelength"].to_numpy())
-        field = Field(axes_series=[wl],name="Absorbance", unit_name="a.u.", data=df["Absorbance"].to_numpy())
-        
+
+        wl = DataSeries(name="Wavelength", unit_name="nm",
+                        data=df["Wavelength"].to_numpy())
+        field = Field(axes_series=[wl], name="Absorbance",
+                      unit_name="a.u.", data=df["Absorbance"].to_numpy())
+
         return cls.from_field(field, **kwargs)
